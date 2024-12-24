a5a871c9666586b676b5f4883571b76c4ec1634f
Blake Pat <blakepat@me.com>
2024-12-24 14:53:00 -0500

    Login and signup screens created with FB
    Firebase db/auth functions working.

--- app/src/main/java/com/example/taskjoy/CreateAccountActivity.kt
+++ app/src/main/java/com/example/taskjoy/CreateAccountActivity.kt
@@ -1,19 +1,111 @@
 package com.example.taskjoy
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.taskjoy.databinding.ActivityMainBinding
+import com.example.taskjoy.databinding.CreateAccountScreenBinding
+import com.example.taskjoy.model.Parent
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 class CreateAccountActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: CreateAccountScreenBinding
+    private lateinit var auth: FirebaseAuth
+
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = CreateAccountScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Create Account"
+
+        setupClickListeners()
+    }
+
+
+
+    private fun setupClickListeners() {
+        binding.btnSignup.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val name = binding.etName.text.toString()
+            val passwordOne = binding.etPasswordOne.text.toString()
+
+            if (formsFilledOutCorrectly()) {
+                createAccount(email, passwordOne, name)
+            }
+        }
+    }
+
+
+    private fun formsFilledOutCorrectly(): Boolean {
+        val email = binding.etEmail.text.toString()
+        val name = binding.etName.text.toString()
+        val passwordOne = binding.etPasswordOne.text.toString()
+        val passwordTwo = binding.etPasswordTwo.text.toString()
+
+        if (email.isEmpty() or name.isEmpty() or passwordOne.isEmpty() or passwordTwo.isEmpty()) {
+            Snackbar.make(binding.root, "Please ensure all fields are filled out", Snackbar.LENGTH_SHORT).show()
+            return false
+        } else if ((passwordOne.count() < 6) or (passwordOne.count() > 20)) {
+            Snackbar.make(binding.root, "Please ensure password meets requirements", Snackbar.LENGTH_SHORT).show()
+            return false
+        } else if (passwordOne != passwordTwo) {
+            Snackbar.make(binding.root, "Passwords do not match", Snackbar.LENGTH_SHORT).show()
+            return false
+        } else {
+            return true
+        }
+    }
+
+
+
+
+    private fun createAccount(email: String, password: String, name: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                val firebaseUser = auth.currentUser
+                val user = Parent(
+                    id = firebaseUser?.uid ?: "",
+                    email = email,
+                    name = name,
+                    children = listOf()
+                )
+
+                firebaseUser?.uid?.let { id ->
+                    db.collection("parents")
+                        .document(id)
+                        .set(user)
+                        .addOnSuccessListener {
+                            finish()
+                        }
+                        .addOnFailureListener { e ->
+                            Snackbar.make(binding.root, "Failed to create user $e ", Snackbar.LENGTH_SHORT).show()
+                        }
+                }
+            }
+            .addOnFailureListener { e ->
+                Snackbar.make(binding.root, "Failed to create authorization $e", Snackbar.LENGTH_SHORT).show()
+            }
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/taskjoy/LoginActivity.kt
+++ app/src/main/java/com/example/taskjoy/LoginActivity.kt
@@ -1,16 +1,80 @@
 package com.example.taskjoy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.taskjoy.databinding.ActivityMainBinding
+import com.example.taskjoy.databinding.LoginScreenBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: LoginScreenBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = LoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        createClickListeners()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        if (auth.currentUser != null) {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun createClickListeners() {
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
+
+        binding.btnSignup.setOnClickListener {
+            signup()
+        }
+
+        binding.btnGuest.setOnClickListener {
+            //TODO: CREATE INTENT TO GO TO THIRD SCREEN
+//            val intent = Intent(this@LoginActivity, CreateAccountActivity::class.java)
+//            startActivity(intent)
+        }
+    }
+
+
+    private fun login() {
+        if (binding.etEmail.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()) {
+            val emailFromUI = binding.etEmail.text.toString()
+            val passwordFromUI = binding.etPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(emailFromUI, passwordFromUI)
+                .addOnCompleteListener(this) {
+                        task ->
+                    if (task.isSuccessful) {
+                        Snackbar.make(binding.root, "Login Successful", Snackbar.LENGTH_SHORT).show()
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Snackbar.make(binding.root, "Login Failed", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            Snackbar.make(binding.root, "Ensure both forms are filled out", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun signup() {
+        val intent = Intent(this@LoginActivity, CreateAccountActivity::class.java)
+        startActivity(intent)
     }
 }
